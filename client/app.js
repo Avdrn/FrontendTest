db70821ec2973c8f797281f6c84e002537a4289c
Anne-Sophie Vandendooren <anne.vandendooren@gmail.com>
2019-09-15 19:04:12 +0200

    css basics

--- client/app.js
+++ client/app.js
@@ -6,8 +6,10 @@ const searchBtn = document.getElementsByClassName("search-btn")[0]
 const username = document.getElementsByClassName("username-container")[0]
 const name = document.getElementsByClassName("name-container")[0]
 const bio = document.getElementsByClassName("bio-container")[0]
+const img = document.getElementsByClassName("img-container")[0]
 const repositories = document.getElementsByClassName("repositories-container")[0]
 const searchBoxErrorMsg = document.getElementsByClassName("search-box_error-msg")[0]
+const app = document.getElementsByClassName("app")[0]
 
 // API infos
 
@@ -24,24 +26,22 @@ const GetUsers = async(user) => {
 }
 
 const showUsers = () => {
- let errorBox = document.getElementsByClassName("error-box")[0]
 
   GetUsers(inputValue.value).then((res) => {
-    if (errorBox) {
-        searchBoxErrorMsg.removeChild(errorBox)
-    }
+
       if (res.data.message === "Not Found") {
         let div = document.createElement('div')
         div.textContent = "Does not exist"
         div.setAttribute('class', 'error-box')
         searchBoxErrorMsg.appendChild(div)
+        app.removeChild(userInfoContainer)
         return
       } else {
-        name.innerHTML = `Name : <span>${res.data.name}</span>`
-        username.innerHTML = `Username : <span>${res.data.login}</span>`
-        bio.innerHTML = `Bio : <span>${res.data.bio}</span>`
-        repositories.innerHTML = `Number of repositories : <span>${res.data.public_repos}</span>`
-        repositories.appendChild(document.createElement("br"))
+        img.src = `${res.data.avatar_url}`
+        name.innerHTML = `${res.data.name}`
+        username.innerHTML = `@${res.data.login}`
+        bio.innerHTML = `${res.data.bio}`
+        showRepos()
       }
   })
 }
@@ -55,13 +55,61 @@ const getRepos = async(user) => {
 }
 
 const showRepos = () => {
+  let repoTitle = document.createElement("div")
+  repoTitle.setAttribute('class', 'repo-title')
+  repoTitle.innerHTML = "Repositories"
+  repositories.appendChild(repoTitle)
+
   getRepos(inputValue.value).then((res) => {
     res.repos.forEach(element => {
-      console.log(element.full_name)
-      repositories.appendChild(document.createTextNode(element.full_name))
-      repositories.appendChild(document.createTextNode(element.forks_count))
-      repositories.appendChild(document.createTextNode(element.stargazers_count))
-      repositories.appendChild(document.createElement("hr"))
+
+      // Create a div
+
+      let repoElement = document.createElement("div")
+      repoElement.setAttribute('class', 'repo-element')
+      repositories.appendChild(repoElement)  
+
+      // set name of repo
+
+      let repoName = document.createElement("div")
+      repoName.setAttribute('class', 'repo-name')
+      repoElement.appendChild(repoName)
+      repoName.innerHTML = `${element.full_name}`
+
+      // Fork count and image creation
+
+      let repoFork = document.createElement("div")
+      repoFork.setAttribute('class', 'repo-fork')
+      repoElement.appendChild(repoFork)
+      
+      let forkP = document.createElement("p")
+      forkP.setAttribute('class', 'fork-p')
+      forkP.innerHTML = `${element.forks_count}`
+      repoFork.appendChild(forkP)
+
+
+      let forkImg = document.createElement("img")
+      forkImg.setAttribute('class', 'fork-img')
+      forkImg.setAttribute('src', 'https://user-images.githubusercontent.com/17777237/54873012-40fa5b00-4dd6-11e9-98e0-cc436426c720.png')
+      repoFork.appendChild(forkImg)
+
+      // Stars count and image creation
+
+      let repoStars = document.createElement("div")
+      repoStars.setAttribute('class', 'repo-stars')
+      repoElement.appendChild(repoStars)
+
+      let starsP = document.createElement("p")
+      starsP.setAttribute('class', 'stars-p')
+      starsP.innerHTML = `${element.stargazers_count}`
+      repoStars.appendChild(starsP)
+
+
+      let starImg = document.createElement("img")
+      starImg.setAttribute('class', 'star-img')
+      starImg.setAttribute('src', 'https://image.flaticon.com/icons/png/512/130/130188.png')
+      repoStars.appendChild(starImg)
+
     })
   })
 }
@@ -70,6 +118,12 @@ const showRepos = () => {
 // onclick function
 
 searchBtn.onclick = function(){
-showUsers()
-showRepos()
+
+  let errorBox = document.getElementsByClassName("error-box")[0]
+ 
+  if (errorBox) {
+   searchBoxErrorMsg.removeChild(errorBox)
+ }
+
+ showUsers()
 }
