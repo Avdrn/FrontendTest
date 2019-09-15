570cb0252e279dfd9900fa163328d922bd751bf9
Anne-Sophie Vandendooren <anne.vandendooren@gmail.com>
2019-09-15 12:30:11 +0200

    search and error message ok

--- client/app.js
+++ client/app.js
@@ -1,20 +1,21 @@
 
-// Elements from HTML
+// Select element from HTML
+
 const inputValue = document.getElementById("search")
 const searchBtn = document.getElementsByClassName("search-btn")[0]
 const username = document.getElementsByClassName("username-container")[0]
 const name = document.getElementsByClassName("name-container")[0]
 const bio = document.getElementsByClassName("bio-container")[0]
 const repositories = document.getElementsByClassName("repositories-container")[0]
-const errorMessage = document.getElementsByClassName("error-message")[0]
-const searchedText = inputValue.value
+const errorBox = document.getElementsByClassName("error-message")[0]
 
+// API infos
 
-// API information
 const client_id = "Iv1.ac9d51bedd230a1f"
 const client_secret = "524701ddf0dd063868dd7af0a9dc5fe06e0dcb9b"
 const url = "https://api.github.com"
 
+// Get users and display info
 
 const GetUsers = async(user) => {
   const api_call = await fetch(`${url}/users/${user}?client_id=${client_id}&client_secret=${client_secret}`)
@@ -23,13 +24,21 @@ const GetUsers = async(user) => {
 }
 
 const showUsers = () => {
-  GetUsers(searchedText).then((res) => {
-    name.innerHTML = `Name : <span>${res.data.name}</span>`
-    username.innerHTML = `Username : <span>${res.data.login}</span>`
-    bio.innerHTML = `Bio : <span>${res.data.bio}</span>`
-    repositories.innerHTML = `Number of repositories : <span>${res.data.public_repos}</span>`
-    })
+  GetUsers(inputValue.value).then((res) => {
+    if (res.data.message === "Not Found") {
+      errorBox.appendChild(document.createTextNode(`Gah, it's not working!`))
+      return
+    } else {
+      name.innerHTML = `Name : <span>${res.data.name}</span>`
+      username.innerHTML = `Username : <span>${res.data.login}</span>`
+      bio.innerHTML = `Bio : <span>${res.data.bio}</span>`
+      repositories.innerHTML = `Number of repositories : <span>${res.data.public_repos}</span>`
+      repositories.appendChild(document.createElement("br"))
+    }
+  })
 }
+
+// Get repos of user and display info
 
 const getRepos = async(user) => {
   const api_call = await fetch(`${url}/users/${user}/repos?client_id=${client_id}&client_secret=${client_secret}`)
@@ -39,12 +48,8 @@ const getRepos = async(user) => {
 
 const showRepos = () => {
   getRepos(inputValue.value).then((res) => {
-    var repoTitle = document.createElement("h2")
-    repoTitle.innerHTML = "Repositories"
-    repositories.appendChild(repoTitle)
-
     res.repos.forEach(element => {
-      repositories.appendChild(document.createElement("hr"))
+      console.log(element.full_name)
       repositories.appendChild(document.createTextNode(element.full_name))
       repositories.appendChild(document.createTextNode(element.forks_count))
       repositories.appendChild(document.createTextNode(element.stargazers_count))
@@ -53,8 +58,10 @@ const showRepos = () => {
   })
 }
 
-searchBtn.onclick = function(){ 
-  showUsers()
-  showRepos()
-} 
-  
+
+// onclick function
+
+searchBtn.onclick = function(){
+showUsers()
+showRepos()
+}
